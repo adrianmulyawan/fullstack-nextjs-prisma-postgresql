51ecddcc9bb0de4c6a162f2bf73089c59e6e7605
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-14 13:29:49 +0700

    buat halaman menampilkan produk dan get data dari table product menggunakan prisma orm

---
+++ app/products/page.jsx
@@ -0,0 +1,102 @@
+import { PrismaClient } from "@prisma/client";
+
+// > metadata
+export const metadata = {
+  title: 'Products'
+};
+
+// > instance prisma
+const prisma = new PrismaClient();
+
+// > method get all data products from db 
+const getAllProducts = async () => {
+  const res = await prisma.product.findMany({
+    select: {
+      id: true,
+      title: true,
+      price: true,
+      category: true,
+      brandId: true,
+      Brand: true
+    }
+  });
+  return res;
+}
+
+const ProductPage = async () => {
+  const products = await getAllProducts();
+
+  return (
+    <>
+      <div className="container mt-3">
+        <h1>Products Page</h1>
+        <hr />
+
+        {/* Button Section */}
+        <div className="table-products my-2">
+          {/* Button Add Products */}
+          <button type="button" className="btn btn-md btn-outline-primary my-3">
+            Add New Product
+          </button>
+
+          {/* List of Products */}
+          <div className="card p-2">
+            <h3 className="my-3 text-center">
+              List of Products
+            </h3>
+            <table className="table table-bordered table-responsive table-striped">
+              <thead>
+                <tr>
+                  <th scope="col" className="text-center">No</th>
+                  <th scope="col" className="text-center">Brand</th>
+                  <th scope="col" className="text-center">Product Name</th>
+                  <th scope="col" className="text-center">Price</th>
+                  <th scope="col" className="text-center">Category</th>
+                  <th scope="col" className="text-center">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  products.length < 1 ? (
+                    <tr>
+                      <td colSpan={ 6 } className="text-center">
+                        Data Products is Empty!
+                      </td>
+                    </tr>
+                  ) : (
+                    products.map((product, index) => (
+                      <tr key={ product.id }>
+                        <th scope="row" className="text-center">
+                          { index += 1 }
+                        </th>
+                        <td className="text-center">
+                          { product.Brand.name }
+                        </td>
+                        <td className="text-center">
+                          { product.title }
+                        </td>
+                        <td className="text-center">
+                          { product.price }
+                        </td>
+                        <td className="text-center">
+                          { product.category }
+                        </td>
+                        <td className="text-center">
+                          <span className="badge bg-primary">Detail</span>
+                          <span className="badge bg-secondary mx-2">Edit</span>
+                          <span className="badge bg-danger">Delete</span>
+                        </td>
+                      </tr>
+                    ))
+                  )
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductPage;
