015806f924a0e7edf9eab8efe2cf9ed80cfd8e2b
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-14 17:24:57 +0700

    buat api untuk mendapatkan data detail product

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -3,6 +3,24 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// > Method untuk get detail data
+export const GET = async (request, {params}) => {
+  // > parse id from string -> int
+  const parseIdToInt = parseInt(params.id);
+  console.info(parseIdToInt, '=> id yang masuk');
+
+  const product = await prisma.product.findFirst({
+    where: {
+      id: parseIdToInt
+    },
+    include: {
+      Brand: true
+    }
+  });
+
+  return NextResponse.json(product, {status: 200});
+};
+
 // > Method untuk delete product
 export const DELETE = async (request, {params}) => {
   // > parse id from string -> int
